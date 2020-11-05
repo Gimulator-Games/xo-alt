ed6278ac07357bb17c9ec8f409af9d420fc8777d
Ali Mirlou <alimirlou@gmail.com>
2020-11-05 10:37:47 +0330

    Update judge

--- judge.py
+++ judge.py
@@ -1,55 +1,49 @@
-from os import environ
-import logging
+from python.client import DirectorClient
+from python.proto_pb2 import *
 from time import sleep
 from json import loads, dumps
 
-from python.proto_pb2 import *
-
-from client import GimulatorClient
-
-LOGLEVEL = environ.get('LOGLEVEL', 'INFO').upper()
-logging.basicConfig(level=LOGLEVEL)
-logger = logging.getLogger('XO Judge')
-
-players = []  # First element will be player X and second will be player of O
+agents = []
 
 
-def evolve(world: dict, player: str, action: list) -> dict:
-    # Discard action if it wasn't its turn
-    if world['turn'] != player:
-        logger.debug("Action was invalid! It wasn't its turn...")
+def evolve(world: dict, agentName: str, action: list) -> dict:
+    print("starting to evolve world", world)
+
+    if world['turn'] != agentName:
+        print("action is invalid! It isn't its turn...")
         raise ValueError()
 
     if not (0 <= action[0] < 3 and 0 <= action[1] < 3):
-        logger.debug("Action was invalid! Position was out of border...")
+        print("invalid action, range error")
         raise ValueError()
 
-    # Discard action if the position is already full
     if world['world'][action[0]][action[1]] is not None:
-        logger.debug("Action was invalid! Position was full...")
+        print("cell was full")
         raise ValueError()
 
-    logger.debug("Action was okay!")
-
-    if player == players[0]:
+    if agentName == agents[0]:
         world['world'][action[0]][action[1]] = 'X'
-        world['turn'] = players[1]
+        world['turn'] = agents[1]
     else:
         world['world'][action[0]][action[1]] = 'O'
-        world['turn'] = players[0]
+        world['turn'] = agents[0]
 
     return world
 
 
 def check_game_status(world: dict):
     board = world['world']
+
     for i in range(3):
         if board[i][0] == board[i][1] == board[i][2]:
             return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i]:
+            return board[0][i]
+
     if board[0][0] == board[1][1] == board[2][2]:
-        return board[0][0]
+        return board[1][1]
     elif board[0][2] == board[1][1] == board[2][0]:
-        return board[0][2]
+        return board[1][1]
     else:
         for row in board:
             for cell in row:
@@ -58,40 +52,38 @@ def check_game_status(world: dict):
     return 'draw'
 
 
-if __name__ == '__main__':
-    client = GimulatorClient()
+if __name__ == "__main__":
+    client = DirectorClient()
 
-    logger.debug("Waiting for players to join...")
     while True:
-        registers = list(client.GetAll(Key(Type='register', Namespace='XO-namespace')))
-        if len(registers) == 2:
-            for player in registers:
-                players.append(player.Content)
+        users = list(client.GetActors())
+        if len(users) == 2:
+            for agent in users:
+                agents.append(agent.Name)
             break
-        sleep(3)
+        sleep(2)
 
-    logger.debug("Creating initial world...")
-    client.Put(Message(Content=dumps({
+    world = {
         'world': [[None] * 3] * 3,
-        'turn': players[0]
-    }), Key=Key(Type='world', Name='judge', Namespace='XO-namespace')))
+        'turn': agents[0],
+    }
+    client.Put(Message(
+        Key=Key(Type="world", Name="referee", Namespace="xo-namespace"),
+        Content=dumps(world),
+    ))
 
-    for action in client.Watch(Key(Type='action', Namespace='XO-namespace')):
-        logger.debug("Action received: " + action.Content)
-
-        world = loads(client.Get(Key(Type='world', Name='judge', Namespace='XO-namespace')).Content)
+    for action in client.Watch(Key(Type="action", Namespace="xo-namespace")):
+        world = loads(client.Get(Key(Type="world", Name="referee", Namespace="xo-namespace")).Content)
 
         try:
-            world = evolve(world, action.Key.Name, loads(action.Content))
+            newWorld = evolve(world, action.Key.Name, loads(action.Content))
         except ValueError:
             continue
 
-        logger.debug("World changed: " + str(world))
-
-        winner = check_game_status(world)
+        winner = check_game_status(newWorld)
         if winner is None:
-            client.Put(Message(Content=dumps(world), Key=Key(Type='world', Name='judge', Namespace='XO-namespace')))
+            client.Put(Message(Key=Key(Type="world", Name="referee", Namespace="xo-namespace"), Content=dumps(newWorld)))
         else:
-            logger.debug("Result of the game: " + winner)
-            client.Put(Message(Content=winner, Key=Key(Type='result', Name='judge', Namespace='XO-namespace')))
-            exit(1)
+            client.PutResult(Result(Status=Status, Score=Score()))
+            print("result: ", winner)
+            exit(0)
